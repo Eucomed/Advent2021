4beffca074e3b63f2310c86b3bf75e518406c99a
Eva Hostakova <eva.hostakova@sigp.eu>
2021-12-02 23:02:30 +0100

    First solution completely done!

--- src/main/java/advent/Solution01.java
+++ src/main/java/advent/Solution01.java
@@ -1,12 +1,19 @@
 package advent;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 
 public class Solution01 {
+    String inputData = RestAssured.given().cookie("session", "53616c7465645f5fe36b6c1894c468c978abdcfa1a7999583cc033e70bac342c8f7cc9a1838169f9d46ee812826719f1").when().get("https://adventofcode.com/2021/day/1/input").then().
+            extract().asString();
+    String[] inputArray = inputData.split("\n");
 
     public static void main(String[] args) {
-        Solution01.test();
+        Solution01 solution1 = new Solution01();
+        Solution01.solve1(solution1.inputArray);
+        Solution01.solve2(solution1.inputArray);
+
+        Solution01.test1();
+        Solution01.test2();
 
         //String[] inputs = AdventBroker.loadInputs(1, 1);
         //String result = Solution01.solve(inputs);
@@ -14,56 +21,51 @@ public class Solution01 {
     }
 
     //private - I don't want to call it from another class
-    private static String solve(String[] inputs) {
+    private static String solve1(String[] inputs) {
         String result = "";
 
-        //  load row
-        String inputData = RestAssured.given().cookie("session", "53616c7465645f5fe36b6c1894c468c978abdcfa1a7999583cc033e70bac342c8f7cc9a1838169f9d46ee812826719f1").when().get("https://adventofcode.com/2021/day/1/input").then().
-                extract().asString();
-        String[] inputArray = inputData.split("\n");
-
         //  convert string to number
-        int[] inputNumbers = new int[inputArray.length];
-        for (int i = 0; i < inputArray.length; i++) {
-            inputNumbers[i] = Integer.parseInt(inputArray[i]);
+        int[] inputNumbers = new int[inputs.length];
+        for (int i = 0; i < inputs.length; i++) {
+            inputNumbers[i] = Integer.parseInt(inputs[i]);
         }
 
-        // set counter == 0
         int counter = 0;
-
-        //  compare to previous number
-        //  if higher, increment counter
-        for (int x = 0; x < inputArray.length - 1; x++) {
-            if (inputNumbers[x + 1] > inputNumbers[x]) {
-                counter += 1;
-            }
-        }
-        System.out.println(counter);
-        // convert counter to string
-        result = Integer.toString(counter);
-
-        // return
+        int counterValue = countIncreasedMeasurement(inputNumbers);
+        System.out.println(counterValue);
+        result = Integer.toString(counterValue);
         return result;
     }
 
-//    public static int countIncreasedMeasurement(String[] input, int[] numbers){
-//        // set counter == 0
-//        int counter = 0;
-//
-//        //  compare to previous number
-//        //  if higher, increment counter
-//        for (int x = 0; x < input.length - 1; x++) {
-//            if (numbers[x + 1] > numbers[x]) {
-//                counter += 1;
-//            }
-//        }
-//        System.out.println(counter);
-//        return counter;
-//
-//    }
+    private static String solve2(String[] inputs) {
+        String counter;
+        int[] finalArray = new int[inputs.length];
+        for (int x = 0; x < finalArray.length - 2; x++) {
+            finalArray[x] = Integer.parseInt(inputs[x]) + Integer.parseInt(inputs[x + 1]) + Integer.parseInt(inputs[x + 2]);
+        }
+        int counterValue = countIncreasedMeasurement(finalArray);
+        System.out.println(counterValue);
+        counter = Integer.toString(counterValue);
+        return counter;
+    }
 
-    public static void test() {
+    public static int countIncreasedMeasurement(int[] input) {
+        int counter = 0;
+        for (int x = 0; x < input.length - 1; x++) {
+            if (input[x + 1] > input[x]) {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+
+    public static void test1() {
         String[] testData = {"199", "200", "208", "210", "200", "207", "240", "269", "260", "263"};
-        assert (solve(testData) == "7") : "test failed! fix your shit!";
+        assert (solve1(testData) == "7") : "Test1 failed! fix your shit!";
+    }
+
+    public static void test2() {
+        String[] testData = {"199", "200", "208", "210", "200", "207", "240", "269", "260", "263"};
+        assert (solve2(testData) == "5") : "Test2 failed! fix your shit!";
     }
 }
